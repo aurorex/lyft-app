b42f309fead73079d269e63889c6dfca39568e45
aurorex <selva_beat@hotmail.com>
2017-12-31 19:40:29 +0100

    subiendo archivos html, con estructura

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+/* el setTimeout direcciona a la vista principal despues de 3 segundos */
+  setTimeout(function() { 
+    window.location.href = 'views/init.html';
+  }, 3000);
+});
+  
